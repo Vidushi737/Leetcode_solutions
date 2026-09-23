504d10ffc54e9a0fffab9e538efbf0a2e9e2caef
Vidushi Gupta <vidushigupta737@gmail.com>
2026-09-24 00:43:30 +0530

    Time: 3 ms (38.80%) | Memory: 14.2 MB (66.41%) - LeetSync

--- 349-intersection-of-two-arrays/intersection-of-two-arrays.cpp
+++ 349-intersection-of-two-arrays/intersection-of-two-arrays.cpp
@@ -5,9 +5,6 @@ public:
         set<int> st;
         set<int> common;
 
-       int v=nums1.size();
-       int e=nums2.size();
-        int l=v+e;
         for(int i=0;i<nums1.size();i++)
         {
             st.insert(nums1[i]);
@@ -15,16 +12,18 @@ public:
         }
         for(int i=0;i<nums2.size();i++)
         {
-           if(st.find(nums2[i])!=st.end())
+           if(st.find(nums2[i])!=st.end())// does the number exist inside the nums1 because we need it to be common between both
            {
-             common.insert(nums2[i]);
+             end.push_back(nums2[i]);
+             st.erase(nums2[i]);
            }
         }
-        for(auto it:common)
+        /*for(auto it:common)
         {
             end.push_back(it);
-        }
+        }*/
         return end;
+        
 
     }
 };
